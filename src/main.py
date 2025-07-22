d79c63bc47e5db07dc5b657322a8293ba51a0838
Владислав <maksimsraud@gmail.com>
2025-07-22 15:44:39 +0300

    feat: added core functionality

--- src/main.py
+++ src/main.py
@@ -2,11 +2,53 @@ import os
 import asyncio
 import requests
 from urllib.parse import unquote
+from datetime import datetime
+from math import floor
 
 from pyrogram import Client
 from pyrogram.raw.functions.messages import RequestAppWebView
 from pyrogram.raw.functions.users import GetUsers
 from pyrogram.raw.types import InputBotAppShortName, InputUser
+
+from typing import List, Optional
+from pydantic import BaseModel, HttpUrl, TypeAdapter
+
+
+class CollectionPreview(BaseModel):
+    name: str
+    url: HttpUrl
+
+
+class Collection(BaseModel):
+    id: str
+    name: str
+    short_name: str
+    preview: CollectionPreview
+
+
+class TopOrderCollection(BaseModel):
+    name: str
+    photo_url: HttpUrl
+    short_name: str
+    floor_price: float
+
+
+class TopOrder(BaseModel):
+    id: str
+    collection_id: str
+    amount: float
+    max_nfts: int
+    current_nfts: int
+    status: str
+    created_at: datetime
+    updated_at: Optional[datetime]
+    expires_at: Optional[datetime]
+    collection: TopOrderCollection
+
+
+collections_adapter = TypeAdapter(List[Collection])
+top_order_adapter = TypeAdapter(TopOrder)
+
 
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
@@ -14,7 +56,7 @@ client = Client('main', api_id=api_id, api_hash=api_hash)
 API_URL = 'https://portals-market.com/api'
 
 
-async def get_auth_token(client: Client):
+async def get_auth_token(client: Client) -> str:
     async with client:
         peer = await client.resolve_peer("portals")
         user_full = await client.invoke(GetUsers(id=[peer]))
@@ -36,19 +78,57 @@ async def get_auth_token(client: Client):
         return f"tma {init_data}"
 
 
-async def main():
-    token = await get_auth_token(client)
+def get_collections(token: str) -> List[Collection]:
+    url = f'{API_URL}/collections/filters/preview?owned_only=false'
     headers = {'Authorization': token}
 
     try:
-        response = requests.get(
-            f'{API_URL}/nfts/search?offset=0&limit=100',
-            headers=headers)
+        response = requests.get(url, headers=headers)
         response_json = response.json()
-        gifts = response_json['results']
-        print(gifts)
+        collections = response_json['collections']
+
+        return collections_adapter.validate_python(collections)
     except Exception as error:
         print(error)
+
+
+def get_collection_top_order(collection_id: str, token: str)\
+        -> TopOrder | None:
+    url = f'{API_URL}/collection-offers/{collection_id}/top'
+    headers = {'Authorization': token}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+
+        if len(response_json):
+            order = response_json[0]
+            return top_order_adapter.validate_python(order)
+
+    except Exception as error:
+        print(error)
+
+
+async def main():
+    token = await get_auth_token(client)
+    collections = get_collections(token)
+
+    for collection in collections:
+        order = get_collection_top_order(collection.id, token)
+        if not order:
+            continue
+
+        order_price = floor((order.amount * 0.95) * 100) / 100
+        price_difference = order_price - order.collection.floor_price
+
+        if price_difference >= 0.1:
+            print(
+                f'''
+                    🏷️{collection.name}
+                    💲Order price: {order_price}
+                    💰Floor price: {order.collection.floor_price}
+                    💸Profit: {price_difference}
+                ''')
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
